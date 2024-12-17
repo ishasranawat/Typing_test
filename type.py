50d5216738e2c46d7615304339abd0777df05f39
Isha <158483807+ishasranawat@users.noreply.github.com>
2024-12-17 12:47:36 +0530

    Update type.py

--- type.py
+++ type.py
@@ -61,7 +61,6 @@ def wpm_test(stdscr):
 		elif len(current_text) < len(target_text):
 			current_text.append(key)
 
-
 def main(stdscr):
 	curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
 	curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
